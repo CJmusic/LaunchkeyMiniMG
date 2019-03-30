0c1173f3ea161489bdda18511f18bc741703addb
Christopher Zaworski <christopherzaworski@gmail.com>
2019-03-30 14:05:05 -0400

    finished the script

--- LaunchkeyMiniMG.py
+++ LaunchkeyMiniMG.py
@@ -18,31 +18,12 @@ from Launchkey.Launchkey import make_encoder
 # copied from InputControlElement for debugging
 MIDI_NOTE_TYPE = 0
 MIDI_CC_TYPE = 1
-#
-
-
-#def make_encoder(channel, cc_no, name):
-#    encoder = EncoderElement(MIDI_CC_TYPE, channel, cc_no, Live.MidiMap.MapMode.absolute)
-#    encoder.set_feedback_delay(-1)
-#    encoder.name = name
-#    return encoder
 
 
 KNOBS = [21, 22, 23, 24, 25, 26, 27, 28] # these are the midi control values for the knobs of the LK Mini 
 PADS = [[40, 41, 42, 43, 48, 49, 50, 51], 
         [36, 37, 38, 39, 44, 45, 46, 47]] # this is the midi note values of the grid of 16 buttons arranged by row/column  
 SCENES = [108, 109] # midi controller values for the two scene launch buttons 
-
-MIDI_CHANNELS = [9, 10, 11, 12, 13,14, 15, 16]
-
-#class LaunchkeyMiniControlFactory(LaunchkeyControlFactory):
-#
-#    def create_next_track_button(self):
-#        return make_button(107, 'Next_Track_Button')
-#
-#    def create_prev_track_button(self):
-#        return make_button(106, 'Prev_Track_Button')
-
 
 class LaunchkeyMiniMG(ControlSurface):
     """ Script for Novation's Launchkey Mini keyboard """
@@ -59,36 +40,23 @@ class LaunchkeyMiniMG(ControlSurface):
         with self.component_guard():
             self._MG_setup()
 
-   # def _setup_navigation(self):
-   #     super(LaunchkeyMini, self)._setup_navigation()
-   #     self._next_scene_button = make_button(105, 'Next_Scene_Button')
-   #     self._prev_scene_button = make_button(104, 'Prev_Scene_Button')
-   #     self._session_navigation.set_next_scene_button(self._next_scene_button)
-   #     self._session_navigation.set_prev_scene_button(self._prev_scene_button)
-
-   # def _setup_transport(self):
-   #     pass
-
     def _MG_setup(self): 
         mixer = MixerComponent(8)
         mixer.set_track_offset(0) #Sets start point for mixer strip (offset from left)
         transport = TransportComponent()
         mixer.set_track_offset(0)
-        #for j in xrange(len(MIDI_CHANNELS)):
         
-        for i in xrange(len(KNOBS)): 
+        volume_knob = SliderElement(MIDI_CC_TYPE, 0, KNOBS[i])
+        pan_knob = SliderElement(MIDI_CC_TYPE, 1, KNOBS[i])
 
-          volume_knob = SliderElement(MIDI_CC_TYPE, 0, KNOBS[i])
-          pan_knob = SliderElement(MIDI_CC_TYPE, 1, KNOBS[i])
+        send_a = SliderElement(MIDI_CC_TYPE, 2, KNOBS[i])
+        send_b = SliderElement(MIDI_CC_TYPE, 3, KNOBS[i])
+        send_c = SliderElement(MIDI_CC_TYPE, 4, KNOBS[i])
+        send_d = SliderElement(MIDI_CC_TYPE, 5, KNOBS[i])
+        send_e = SliderElement(MIDI_CC_TYPE, 6, KNOBS[i])
+        send_f = SliderElement(MIDI_CC_TYPE, 7, KNOBS[i])
 
-
-          send_a = SliderElement(MIDI_CC_TYPE, 2, KNOBS[i])
-          send_b = SliderElement(MIDI_CC_TYPE, 3, KNOBS[i])
-          send_c = SliderElement(MIDI_CC_TYPE, 4, KNOBS[i])
-          send_d = SliderElement(MIDI_CC_TYPE, 5, KNOBS[i])
-          send_e = SliderElement(MIDI_CC_TYPE, 6, KNOBS[i])
-          send_f = SliderElement(MIDI_CC_TYPE, 7, KNOBS[i])
-
+        for i in xrange(len(KNOBS)): #set the functions of the knobs
           mixer.channel_strip(i).set_volume_control(volume_knob)
           mixer.channel_strip(i).set_pan_control(pan_knob)
           mixer.channel_strip(i).set_send_controls([send_a,
@@ -99,12 +67,11 @@ class LaunchkeyMiniMG(ControlSurface):
                                                     send_f])
 
           # scenes are locked to channel 14
-          transport.set_overdub_button(ButtonElement(False, MIDI_CC_TYPE, 0, SCENES[0]))
-          transport.set_stop_button(ButtonElement(False, MIDI_CC_TYPE, 0, SCENES[1]))                 
-
+        transport.set_overdub_button(ButtonElement(False, MIDI_CC_TYPE, 0, SCENES[0]))
+        transport.set_stop_button(ButtonElement(False, MIDI_CC_TYPE, 0, SCENES[1]))                 
           #transport.set_play_button(ButtonElement(False, MIDI_CC_TYPE, 1, SCENES[0]))
           #transport.set_stop_button(ButtonElement(False, MIDI_CC_TYPE, 1, SCENES[1]))                 
-                                                                                                           
+          # code left above so you cant replace either button with a play button
                                                                                                            
                                                                                                            
     def disconnect(self):
